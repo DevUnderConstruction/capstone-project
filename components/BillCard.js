4e403eecdd95a720eea0b6417c371d18b32c0d41
Jonas Meidinger <jomeidinger@gmx.de>
2023-01-14 15:28:15 +0100

    add BillCard which accepts customers and an onclick on Usercard which renders it

---
+++ components/BillCard.js
@@ -0,0 +1,10 @@
+export default function BillCard({ customers }) {
+  return (
+    <label>
+      select a customer:
+      <select>
+        <option>please select</option>
+      </select>
+    </label>
+  );
+}
